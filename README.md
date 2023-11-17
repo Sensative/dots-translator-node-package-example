56de201e0c25c15ab32d72193789dae3dc4d422a
James Holmquist <james.holmquist@sensative.com>
2023-11-17 16:50:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dots-translator-node-package-example
 Example of how to use the vsm-translator-open-source via NPM, in this case for command-line translation
+
+- Usage: node cliTranslator.js <port> <data> <rule crc>
+    `example: node cliTranslator.js 1 00ab 57715230`
---
+++ cliTranslator.js
@@ -0,0 +1,45 @@
+// Very basic example use of translator node package
+
+//
+// Usage: node cliTranslator.js <port> <data> <rule crc>
+// example: node cliTranslator.js 1 00ab 57715230
+
+//
+// Require 
+// VSM Translator 
+const translator = require('vsm-translator');
+// CLI parser
+const args = require('minimist')(process.argv.slice(2));
+
+//
+// Constructing "fake" iotnode Object from command line arguments
+let loraPort = args._[0]; // Port LoRa message was sent on
+let loraHexData = args._[1]; // Message data in hex
+let ts = 1 // Timestamp input not implemented, but input unix time format for output timestamps to make sense
+let size = 256 // LoRa package size, should be grabbed from DR. Needed for large messages that are split up like WiFi etc to work properly.
+let rules = args._[2]; // Rules CRC can be found by translating a port 15 message with any crc. Ideally should be saved for each individual node
+
+let iotnode = { 
+    encodedData : {
+      port : loraPort,
+      hexEncoded : loraHexData,
+      timestamp : ts,
+      maxSize : size,
+    },
+    vsm : {
+      rulesCrc32 : rules,
+    },
+  }
+
+//
+// Run constructed iotnode through translator
+let result = {}
+try {
+  const returned = translator.translate(iotnode);
+  result = returned.result;
+} catch (e) {
+  console.log("Failed translation: ", e.message);
+}
+
+// Log result into console
+console.log(result)
