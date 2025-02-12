f27c13a2a77971aefbcc9c1eb3c3076dc255990b
James Holmquist <james.holmquist@sensative.com>
2025-02-12 12:05:40 +0100

    Updated to latest npm translator

--- cliTranslator.js
+++ cliTranslator.js
@@ -11,7 +11,7 @@ const args = require('minimist')(process.argv.slice(2), { string: ['_'] });
 // Constructing "fake" iotnode Object from command line arguments
 let loraPort = args._[0]; // Port LoRa message was sent on
 let loraHexData = args._[1]; // Message data in hex
-let ts = 1 // Timestamp input not implemented, but input unix time format for output timestamps to make sense
+let ts = Date.now() // Timestamp input not implemented, but input unix time format for output timestamps to make sense
 let size = 256 // LoRa package size, should be grabbed from DR. Needed for large messages that are split up like WiFi etc to work properly.
 let rules = args._[2]; // Rules CRC can be found by translating a port 15 message with any crc. Ideally should be saved for each individual node
 
